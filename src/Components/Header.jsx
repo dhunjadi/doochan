8a2d4d7243e1664d3f81341a1bf5b5fc520ab1ef
dhunjadi <74426940+dhunjadi@users.noreply.github.com>
2021-07-05 19:51:45 +0200

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div id='header'>
+            <div className="logo"><h1>Doochan</h1></div>
+        </div>
+    )
+}
