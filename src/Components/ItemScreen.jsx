32124b939fdc2e74a50873da9a3af9c62753514c
dhunjadi <74426940+dhunjadi@users.noreply.github.com>
2021-07-21 21:12:20 +0200

    Added Item page CSS

--- src/Components/ItemScreen.jsx
+++ src/Components/ItemScreen.jsx
@@ -5,21 +5,32 @@ import { useParams } from 'react-router'
 export default function ItemScreen(props) {
 
     const { id } = useParams();
-    const item = itemList[`${id-1}`]
+    const item = itemList[`${id - 1}`]
 
 
     return (
-        <div id='product-screen'>
-            <div className='product-img-div'>
-                <div className="product-img"> <img src={item.img} alt="" /> </div>
-            </div>
-            <div className="product-details">
-                <p>{item.title}</p>
-                <p>{item.description}</p>
-                <div className="buttons">
-                    <button>Buy</button>
-                    <button>Favorite</button>
+        <div id='item-screen'>
+            <div className='item-img-div'>
+                <div className="item-img"> <img src={item.img} alt="" /> </div>
+                <div className="item-price">
+                    <p>$9.99</p>
                 </div>
+                <div className="item-details">
+                    <p className='item-title'>{item.title}</p>
+                    <p className='item-description'>{item.description}</p>
+                    <div className="buttons">
+                        <button className='add-to-cart-btn-item'>Add To Cart</button>
+                        <button className='fav-btn-item'>Favorite</button>
+                    </div>
+                </div>
+            </div>
+            <div className="more-pictures-div">
+                <div className="more-pictures"> <img className='more-pictures-pic' src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" /> </div>
+                <div className="more-pictures"> <img className='more-pictures-pic' src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" /> </div>
+                <div className="more-pictures"> <img className='more-pictures-pic' src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" /> </div>
+                <div className="more-pictures"> <img className='more-pictures-pic' src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" /> </div>
+                <div className="more-pictures"> <img className='more-pictures-pic' src="https://www.pictureframesexpress.co.uk/blog/wp-content/uploads/2020/05/7-Tips-to-Finding-Art-Inspiration-Header-1024x649.jpg" alt="" /> </div>
+
             </div>
         </div>
     )
