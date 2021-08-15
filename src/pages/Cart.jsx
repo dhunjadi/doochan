052fb27f31caf158e5bbf97b0e4ac21e8eb30202
dhunjadi <74426940+dhunjadi@users.noreply.github.com>
2021-08-15 19:22:15 +0200

    Items are now shown in the Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,6 +9,16 @@ export default function Cart() {
     return (
         <div>
             <h1>Cart</h1>
+            {cart.map((cartItem) => (
+                  <div className='item-div'>
+                      <div className="item-img-div"> <img className='product-img' src={cartItem.img[0].img} alt="" /> </div>
+                      <div onClick={console.log(cartItem)} className="item-title"> <h3>{cartItem.title}</h3> </div>
+                      <div className="item-description"> <p>{cartItem.description}</p> </div>
+                      <div className="add-to-cart-div">
+                          <span>{cartItem.price}</span>
+                      </div>
+              </div>
+            ))}
         </div>
     )
 }
