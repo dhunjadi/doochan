65be829b4ab0fab6fddb42454e96bc684faa8e4e
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-17 13:56:29 +0200

    added pagination

--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -1,21 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "../Components/context/DataContext";
 import Item from "../Components/Item";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import ReactPaginate from "react-paginate";
 
 export default function Art() {
-  const { fetched } = useContext(DataContext)
+  const { fetched } = useContext(DataContext);
+  const [pageNumber, setPageNumber] = useState(0);
+
   const filtered = fetched.filter((item) => {
     return item.section === "Art";
   });
 
-  const createItem = (item) => {
-    return <Item key={uuidv4()} item={item} />;
+  const itemsPerPage = 6;
+  const pagesVisited = pageNumber * itemsPerPage;
+
+  const displayItems = filtered
+    .slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map((item) => {
+      return <Item key={uuidv4()} item={item} />;
+    });
+
+  const pageCount = Math.ceil(filtered.length / itemsPerPage);
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected);
   };
 
   return (
     <div id="art">
-      <div className="item-container">{filtered.map(createItem)}</div>
+      <div className="item-container">{displayItems}</div>
+      <ReactPaginate 
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination-container"}
+          previousLinkClassName={"previous-btn"}
+          nextLinkClassName={"next-btn"}
+          disabledClassName={"pagination-disabled"}
+          activeClassName={"pagination-active"}
+        />
     </div>
   );
 }
--- src/pages/Toys.jsx
+++ src/pages/Toys.jsx
@@ -1,21 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "../Components/context/DataContext";
 import Item from "../Components/Item";
 import { v4 as uuidv4 } from 'uuid';
+import ReactPaginate from "react-paginate";
 
 export default function Toys() {
   const { fetched } = useContext(DataContext);
+  const [pageNumber, setPageNumber] = useState(0);
+
   const filtered = fetched.filter((item) => {
     return item.section === "Toys";
   });
 
-  const createItem = (item) => {
-    return <Item key={uuidv4()} item={item} />;
+  const itemsPerPage = 6;
+  const pagesVisited = pageNumber * itemsPerPage;
+
+  const displayItems = filtered
+    .slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map((item) => {
+      return <Item key={uuidv4()} item={item} />;
+    });
+
+  const pageCount = Math.ceil(filtered.length / itemsPerPage);
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected);
   };
 
   return (
-    <div id="toy">
-      <div className="item-container">{filtered.map(createItem)}</div>
+    <div id="art">
+      <div className="item-container">{displayItems}</div>
+      <ReactPaginate 
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination-container"}
+          previousLinkClassName={"previous-btn"}
+          nextLinkClassName={"next-btn"}
+          disabledClassName={"pagination-disabled"}
+          activeClassName={"pagination-active"}
+        />
     </div>
   );
 }
